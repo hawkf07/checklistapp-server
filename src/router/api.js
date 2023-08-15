da768a56b8c472d3db7bb4f0ea77dce5876bd8c0
hawkf07 <fikrfifadilah299@gmail.com>
2023-08-15 21:14:35 +0700

    fix: login auth error because sql

--- src/router/api.js
+++ src/router/api.js
@@ -1,50 +1,64 @@
 import { getUser, isAuthJWT } from "../utils/index.js";
 import { Router } from "express";
+import jwt from "jsonwebtoken";
 const router = Router();
 import bcrypt from "bcrypt";
+import { users } from "../db/schema.js";
+import { db } from "../db/index.js";
 
 router.post("/login", async (req, res, next) => {
-  const { username, email, password } = req.body;
-  const allUsers = await getUser(username || email);
+  try {
+    const { username, email, password } = req.body;
+    const allUsers = await getUser(username || email);
 
-  const user = allUsers[0];
-  if (user == undefined) {
-    res.send("No user founded");
+    const user = allUsers[0];
+    if (!user) {
+      res.status(400).send("No user found!");
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.send({ message: "password not match" });
+    }
+    const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
+    res
+      .cookie("token", token, { httpOnly: false, maxAge: 1000 * 60 * 10 })
+      .send({
+        message: "Login Successfully",
+        status: 200,
+        id: user.id,
+      });
+  } catch (error) {
+    res.status(400).send({
+      error,
+    });
   }
-  const isMatch = await bcrypt.compare(password, user.password);
-
-  if (!isMatch) {
-    res.send({ message: "password not match" });
-  }
-
-  const token = jwt.sign(user, process.env.JWT_SECRET, { expiresIn: "1h" });
-  res
-    .cookie("token", token, { httpOnly: false, maxAge: 1000 * 60 * 10 })
-    .send("cookie send!");
 });
 
 router.post("/signup", async (req, res, next) => {
   try {
     const { username, email, password } = req.body;
-    console.log(req.body, " is body");
     const hashedPassword = await bcrypt.hash(password, 10);
     await db
       .insert(users)
       .values({
-        email,
         password: hashedPassword,
         username,
+        email,
       })
       .then((value) =>
-        res.status(200).send({ message: "user successfully created" })
+        res
+          .status(200)
+          .send({ message: "user successfully created", status: 200 })
       )
       .catch((error) => res.send(error));
   } catch (error) {
-    return res.status(400).json(error);
+    console.log(error.detail);
+    return res.status(400).json({ error, detail: error.detail });
   }
 });
 
 router.post("/logout", (req, res, next) => {
-  res.send("Handle logout");
+  res.clearCookie("token");
 });
-export { router };
+export default router;
--- src/router/functionalities.js
+++ src/router/functionalities.js
@@ -1,4 +1,6 @@
 import { Router } from "express";
+import { db } from "../db/index.js";
+import { todos, users } from "../db/schema.js";
 
 const router = Router();
 
@@ -8,24 +10,31 @@ router.get("/", (req, res) => {
   });
 });
 // add notes
-router.post("/add_notes", (req, res, next) => {});
+router.post("/add_notes", async (req, res, next) => {
+  const { title, description } = req.body;
+
+  try {
+    const notes = await db.insert(users).values(req.body);
+  } catch (error) {
+    res.status(400).json({ error });
+  }
+});
 //get notes
 router.get("/get_notes", (req, res, next) => {
-  console.log(req.user);
   res.status(200).send({
     message: "user is authenticated",
   });
 });
 //delete notes
-router.delete("/delete_notes", (req, res, next) => {});
+router.delete("/delete_notes", (req, res, next) => { });
 // update notes
-router.patch("/update_notes", (req, res, next) => {});
+router.patch("/update_notes", (req, res, next) => { });
 // add todos
-router.post("/add_todos", (req, res, next) => {});
+router.post("/add_todos", (req, res, next) => { });
 //get todos
-router.get("/get_todos", (req, res, next) => {});
+router.get("/get_todos", (req, res, next) => { });
 //delete todos
-router.delete("/delete_todos", (req, res, next) => {});
+router.delete("/delete_todos", (req, res, next) => { });
 // update todos
-router.patch("/update_todos", (req, res, next) => {});
+router.patch("/update_todos", (req, res, next) => { });
 export default router;
